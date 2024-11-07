5b80155725b6302f0b59e79430b1cab5c355397a
yerinahn <annie100.ahn@gmail.com>
2024-11-07 17:28:22 +0900

    Add files via upload

---
+++ netsim/Project1/Commandlist.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <signal.h>
+#include "Gym.h"
+#include "../IP/IP.h"
+#include "../Satellite/Satellite_PHY.h"
+#include"../LTE_NR//LTE_NR.h"
+#include"../LTE_NR/LTENR_GNBRRC.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../CLIInterpretor/CLI.h"
+
+#define MAX_DEVICE_NAMES 1000  // max number of device_namefor saving 
+#define MAX_STRING_LENGTH 2560 // max string length
+
+#define SIZE_OF_MAP 1
+
+
+void process_obs_command(int commandname, char* source){
+	
+	switch (commandname) {
+	case 1:
+		strcpy(source, "RandomState");
+		break;
+	case 2:
+		strcpy(source, "IntState");
+		break;
+	case 3:
+		strcpy(source, "Dummy");
+		break;
+	case 4:
+		strcpy(source, "SINR");
+		break;
+	case 5:
+		strcpy(source, "Throughput");
+		break;
+	case 6:
+		strcpy(source, "SumThroughput");
+		break;
+	case 7:
+		strcpy(source, "UEData");
+		break;
+	case 8:
+		strcpy(source, "CellLoad");
+		break;
+	case 9:
+		strcpy(source, "EdgeUE");
+		break;
+
+	default:
+		source[0] = '\0';
+		break;
+	}
+
+}
+
+void process_act_command(ActMsg* actmsg, ptrCLIENTINFO info, int j) {
+
+	switch (actmsg->command_name) {
+
+	case 1: 
+		apply_random_action(actmsg);
+		break;
+	case 2: 
+		apply_int_action(actmsg);
+		break;
+	case 3:
+		apply_power_action_to_all_gNBs(actmsg);
+		break;
+	case 4:
+		apply_HOM_to_all_gNBs(actmsg);
+		break;
+
+	default:
+		break;
+	}
+
+	// for processing extra doneinfo
+	
+	if (actmsg != NULL && actmsg->doneinfo == TRUE) {
+		cli_stop_simulation(info);
+	}
+	act_msg__free_unpacked(actmsg, NULL);
+}
+
---
+++ netsim/Project1/Reserve.c
@@ -0,0 +1,103 @@
+#include <signal.h>
+#include "Gym.h"
+#include "../CLIInterpretor/CLI.h"
+#include "../IP/IP.h"
+#include "../Satellite/Satellite_PHY.h"
+#include"../LTE_NR//LTE_NR.h"
+#include"../LTE_NR/LTENR_GNBRRC.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool validate_reserve_command(ptrCLIENTINFO info, ptrCOMMANDARRAY command, int index)
+{
+    if (command->length - index < 2)
+    {
+        send_message(info, "USAGE: reserve deviceName\n");
+        return false;
+    }
+
+    if (_stricmp(command->commands[index], "reserve"))
+        return false;
+
+    return true;
+}
+
+void execute_reserve_command(ptrCLIENTINFO info, InitMsg* initmsg, const char* devicename, float totaltime, float timestep)
+{
+    for (int i = 0; i < initmsg->n_command_name; i++) {
+        int commandname = initmsg->command_name[i];
+    
+        char source[100];
+
+        process_obs_command(commandname, source);
+
+        ptrCOMMANDARRAY command = create_command_array(source, devicename); 
+
+  
+        schedule_events(command, info, totaltime, timestep); 
+        commandname = 0;
+    }
+}
+
+void scheduling_pause(ptrCLIENTINFO info, float totaltime, float timestep)
+{
+
+	double timeschedule = 1;
+	while (1) {
+		timeschedule += timestep * 1000000; // time step(s) * 1000000
+		timeschedule = timeschedule / 1000000;
+		cli_pause_simulation_at(info, timeschedule);
+		timeschedule = timeschedule * 1000000;
+		if (timeschedule > totaltime* 1000000)
+			break;
+	}
+}
+
+ptrCOMMANDARRAY create_command_array(const char* source, const char* devicename)
+{
+    size_t source_length = strlen(source);
+    size_t devicename_length = strlen(devicename);
+    size_t total_length = source_length + 1 + devicename_length; 
+
+    // Allocate memory for the command string
+    char* commandstring = NULL;
+    commandstring = (char*)malloc((total_length + 1) * sizeof(char)); 
+    if (commandstring == NULL) {
+        fprintf(stderr, "Memory allocation failed for commandstring\n");
+        return;
+    }
+    snprintf(commandstring, total_length + 1, "%s %s", source, devicename); 
+
+
+    fprintf(stderr, "stirng: %s ", commandstring);
+    // Allocate memory for the COMMANDARRAY structure
+    ptrCOMMANDARRAY command = (ptrCOMMANDARRAY)malloc(sizeof(COMMANDARRAY));
+    command->originalCommand = commandstring;
+    command->commands = (char**)malloc(2 * sizeof(char*));
+
+    // Allocate memory for each command part and copy the strings
+    command->commands[0] = (char*)malloc((source_length + 1) * sizeof(char));
+    command->commands[1] = (char*)malloc((devicename_length + 1) * sizeof(char));
+    strncpy(command->commands[0], source, source_length);
+    command->commands[0][source_length] = '\0';
+    strncpy(command->commands[1], devicename, devicename_length);
+    command->commands[1][devicename_length] = '\0';
+    command->length = 2; 
+
+    return command;
+}
+
+void schedule_events(ptrCOMMANDARRAY command, ptrCLIENTINFO info, float totaltime, float timestep)
+{   
+    for (double time = timestep; time < totaltime; time += timestep) {
+        NetSim_EVENTDETAILS pevent;
+        memset(&pevent, 0, sizeof pevent);
+        pevent.dEventTime = time * SECOND;
+        pevent.nEventType = TIMER_EVENT;
+        pevent.nProtocolId = PROTOCOL_CLI;
+        pevent.nSubEventType = SUBEVENT_EXECUTECOMMAND;
+        pevent.szOtherDetails = FORM_CLI_HANDLE(command, info);
+        fnpAddEvent(&pevent);
+    }
+}
